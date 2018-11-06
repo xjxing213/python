4541c6d955e7f464db23da437409d84bc6d3edda
xjxing213 <39912419+xjxing213@users.noreply.github.com>
2018-11-06 16:43:38 +0800

    自动报时
    按规定时间循环报时

---
+++ Speak/speakHour.py
@@ -0,0 +1,14 @@
+﻿import time,datetime
+import win32com.client
+
+speaker = win32com.client.Dispatch("SAPI.SpVoice")
+while True:
+    minutes=int(str(datetime.datetime.now())[15:16])
+    print str(datetime.datetime.now())[11:19]
+    #每10分钟报时一次
+    if minutes == 0:
+        now=str(datetime.datetime.now())[11:19]
+        str1=u'现在时间是：' + now
+        speaker.Speak(str1)
+        time.sleep(35)
+    time.sleep(30)
